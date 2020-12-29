79c84d50688e15778035673c7ae62a36f1ff5e89
Ben Bramley <ben@velio.io>
2020-12-30 10:40:36 +1100

    added support for providing an executor to the tailer - WIP

--- src/nomis_tailer/core.clj
+++ src/nomis_tailer/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.core.async :as a]
             [nomis-tailer.files :as files])
   (:import (org.apache.commons.io.input Tailer
-                                        TailerListener)))
+                                        TailerListener)
+           (java.util.concurrent Executor)))
 
 ;;;; ___________________________________________________________________________
 ;;;; ---- Implementation ----
@@ -24,15 +25,23 @@
     (^void handle [this ^Exception e]
      (throw e))))
 
-(defn ^:private make-single-file-tailer-impl [file delay-ms from-end?]
-  (let [ch        (a/chan)
-        tailer    (Tailer/create file
-                                 (tailer-listener ch)
-                                 delay-ms
-                                 from-end?)]
-    {:type     :single-file-tailer
-     ::channel ch
-     ::tailer  tailer}))
+(defn ^:private make-single-file-tailer-impl
+  ([file delay-ms from-end?]
+   (let [ch (a/chan)
+         tailer (Tailer/create file
+                               (tailer-listener ch)
+                               delay-ms
+                               from-end?)]
+     {:type     :single-file-tailer
+      ::channel ch
+      ::tailer  tailer}))
+  ([file delay-ms from-end? ^Executor executor]
+   (let [ch (a/chan)
+         tailer (Tailer. file (tailer-listener ch) delay-ms from-end?)]
+     (.execute executor tailer)
+     {:type     :single-file-tailer
+      ::channel ch
+      ::tailer  tailer})))
 
 ;;;; ___________________________________________________________________________
 ;;;; ---- Generic stuff ----
@@ -57,8 +66,10 @@
   "Returns a single-file-tailer for `file`.
   The tailing starts from the end of the file.
   `delay-ms` is the delay between checks of the file for new content."
-  [file delay-ms]
-  (make-single-file-tailer-impl file delay-ms true))
+  ([file delay-ms]
+   (make-single-file-tailer-impl file delay-ms true))
+  ([file delay-ms ^Executor executor]
+   (make-single-file-tailer-impl file delay-ms true executor)))
 
 (defmethod channel :single-file-tailer
   [{:keys [::channel ::tailer]}]
@@ -78,59 +89,62 @@
   The tailing starts from the end of any current file, and includes the
   full content of subsequent files.
   Returns a multi-file-tailer."
-  ([dir pattern delay-ms new-file-check-frequency-ms]
-   (make-multi-file-tailer dir
-                           pattern
-                           delay-ms
-                           new-file-check-frequency-ms
-                           1000))
-  ([dir pattern delay-ms new-file-check-frequency-ms delay-ms-to-finish-old-file]
-   (let [out-ch     (a/chan)
-         control-ch (a/chan)]
-     (letfn [(get-most-recent-file []
-               (files/most-recent-file-matching-pattern dir pattern))
-             (new-tailer [most-recent-file first?]
-               (let [tailer (make-single-file-tailer-impl most-recent-file
-                                                          delay-ms
-                                                          first?)]
-                 (a/go-loop []
-                   (let [v (a/<! (::channel tailer))]
-                     (when v
-                       (a/>! out-ch v)
-                       (recur))))
-                 tailer))]
-       (a/go
-         (let [first-most-recent-file (get-most-recent-file)]
-           (let [first-tailer (when first-most-recent-file
-                                (new-tailer first-most-recent-file
-                                            true))]
-             (loop [prev-most-recent-file first-most-recent-file
-                    prev-tailer           first-tailer]
-               (let [[v ch] (a/alts! [control-ch
-                                      (a/timeout new-file-check-frequency-ms)]
-                                     :priority true)]
-                 (if (= v :stop)
-                   (when prev-tailer
-                     (close! prev-tailer))
-                   (let [most-recent-file     (get-most-recent-file)
-                         new-file-to-process? (and most-recent-file
-                                                   (not= most-recent-file
-                                                         prev-most-recent-file))]
-                     (if-not new-file-to-process?
-                       ;; just leave things as they are
-                       (recur prev-most-recent-file
-                              prev-tailer)
-                       (do (when prev-tailer
-                             ;; Give time to finish tailing the previous file
-                             ;; before closing the channel.
-                             (a/<! (a/timeout  delay-ms-to-finish-old-file))
-                             (close! prev-tailer))
-                           (recur most-recent-file
-                                  (new-tailer most-recent-file
-                                              false))))))))))))
-     {:type        :multi-file-tailer
-      ::channel    out-ch
-      ::control-ch control-ch})))
+  [dir pattern & [opt]]
+  (let [delay-ms (or (:delay-ms opt) 1000)
+        new-file-check-frequency-ms (or (:new-file-check-frequency-ms opt) 10000)
+        delay-ms-to-finish-old-file (or (:delay-ms-to-finish-old-file opt) 1000)
+        executor (:executor opt)
+        out-ch (a/chan)
+        control-ch (a/chan)]
+    (letfn [(get-most-recent-file []
+              (files/most-recent-file-matching-pattern dir pattern))
+            (new-tailer [most-recent-file first?]
+              (let [tailer (if executor
+                             (make-single-file-tailer-impl most-recent-file
+                                                           delay-ms
+                                                           first?
+                                                           executor)
+                             (make-single-file-tailer-impl most-recent-file
+                                                           delay-ms
+                                                           first?))]
+                (a/go-loop []
+                  (let [v (a/<! (::channel tailer))]
+                    (when v
+                      (a/>! out-ch v)
+                      (recur))))
+                tailer))]
+      (a/go
+        (let [first-most-recent-file (get-most-recent-file)]
+          (let [first-tailer (when first-most-recent-file
+                               (new-tailer first-most-recent-file
+                                           true))]
+            (loop [prev-most-recent-file first-most-recent-file
+                   prev-tailer first-tailer]
+              (let [[v ch] (a/alts! [control-ch
+                                     (a/timeout new-file-check-frequency-ms)]
+                                    :priority true)]
+                (if (= v :stop)
+                  (when prev-tailer
+                    (close! prev-tailer))
+                  (let [most-recent-file (get-most-recent-file)
+                        new-file-to-process? (and most-recent-file
+                                                  (not= most-recent-file
+                                                        prev-most-recent-file))]
+                    (if-not new-file-to-process?
+                      ;; just leave things as they are
+                      (recur prev-most-recent-file
+                             prev-tailer)
+                      (do (when prev-tailer
+                            ;; Give time to finish tailing the previous file
+                            ;; before closing the channel.
+                            (a/<! (a/timeout delay-ms-to-finish-old-file))
+                            (close! prev-tailer))
+                          (recur most-recent-file
+                                 (new-tailer most-recent-file
+                                             false))))))))))))
+    {:type        :multi-file-tailer
+     ::channel    out-ch
+     ::control-ch control-ch}))
 
 (defmethod channel :multi-file-tailer
   [{:keys [::channel ::control-ch]}]
